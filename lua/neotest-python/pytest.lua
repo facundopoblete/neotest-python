84b292e399f228be275c00868092d7367dddaa22
Facundo <facundo1792@gmail.com>
2024-01-04 12:49:29 -0300

    fix if and

--- lua/neotest-python/pytest.lua
+++ lua/neotest-python/pytest.lua
@@ -72,7 +72,7 @@ local function discover_params(python, script, path, positions, root)
 
     for i, pos in positions:iter_nodes() do
       if string.find(line, pos:data().name, nil, true) then
-        if (string.find(line, pos:data().name, nil, true) + #pos:data().name) ~= #line #pos:data().name > match_score then
+        if (string.find(line, pos:data().name, nil, true) + #pos:data().name) ~= #line and #pos:data().name > match_score then
           match_score = #pos:data().name
           test = pos
         end
